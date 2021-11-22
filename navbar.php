9ffdbc0962c2ca0ad00bf5c9a0e134aace203ffc
mendekam <abiddddbilal@gmail.com>
2021-11-23 00:35:08 +0700

    handling role admin bisa ke input barang

--- navbar.php
+++ navbar.php
@@ -1,4 +1,3 @@
-
 <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
 	<h2><a class="navbar-brand" href="#">E L J I</a></h2>
 	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
@@ -7,7 +6,9 @@
 	<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
 		<div class="navbar-nav">
 			<a class="nav-item nav-link active" href="index.php">Home<span class="sr-only">(current)</span></a>
+			<?php if(isset($_SESSION["role"]) && $_SESSION["role"] == "admin"):?>
 			<a class="nav-item nav-link" href="input_produk.php">Tambah produk<span class="sr-only">(current)</span></a>
+			<?php endif; ?>
 		</div>
 		<div class="navbar-nav ml-auto">
 			<a class="nav-item nav-link " href="cart.php"><img src="./img/cart.png" width="25px" height="25px"alt=""></a>
